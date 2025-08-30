35721c45096e022efe39fc57637b46ea1f699979
mohamed baza <139691710+eevou@users.noreply.github.com>
2025-08-30 21:53:49 +0300

    Add project files.

---
+++ ConsoleApp/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab03
+{
+    class Employee
+    {
+        // Fields
+        private int id;
+        private string name;
+        private int age;
+        private int salary;
+
+        // Static counter
+        private static int counter = 0;
+        // Properties
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    name = "Unknown";
+                else
+                    name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        // Default Constructor
+        public Employee() { counter++; }
+
+        // Parameterized Constructor
+        public Employee(int id, string name, int age, int salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+            counter++;
+        }
+        // diplay method
+        public void Display()
+        {
+            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}, Salary: {salary}");
+        }
+        // Static method to get count
+        public static int GetCount()
+        {
+            return counter;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace lab03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp1 = new Employee();
+            Console.Write("ID: ");
+            emp1.Id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            emp1.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            emp1.Age = int.Parse(Console.ReadLine());
+            Console.Write("Salary: ");
+            emp1.Salary = int.Parse(Console.ReadLine());
+            emp1.Display();
+            Console.WriteLine($"Total Employees Created: {Employee.GetCount()}");
+
+
+            Employee[] employees = new Employee[3];
+
+            // Read Employees from user
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine($"Enter Employee {i + 1} data:");
+
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Salary: ");
+                int salary = int.Parse(Console.ReadLine());
+
+                employees[i] = new Employee(id, name, age, salary);
+            }
+
+            Console.WriteLine("\nEmployees Data:");
+            foreach (var emp in employees)
+            {
+                emp.Display();
+            }
+
+            Console.WriteLine($"\nTotal Employees Created: {Employee.GetCount()}");
+        }
+    }
+}
+
